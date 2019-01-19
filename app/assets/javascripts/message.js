8254d6d2395733ec78968b208ccc5e1230373e68
Terasawa Kazuyuki <ie2965@hotmail.com>
2019-01-19 19:58:01 +0900

    Edit controller

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('#ID名').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
