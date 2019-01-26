2edd644095476e504908fe0cf05b8da8e888a97e
Terasawa Kazuyuki <ie2965@hotmail.com>
2019-01-26 19:53:15 +0900

    Remove disabled

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,27 @@
 $(function() {
+  function buildHTML(message){
+    var html =`<div class="message">
+                <div class="upper-message">
+                  <div class="upper-message__user-name">
+                  ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                  ${message.created_at}
+                  </div>
+                </div>
+                <div class="lower-meesage">
+                  <p class="lower-message__content">
+                  ${message.content}
+                  </p>
+                  ${message.image}
+                </div>
+              </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).prop('action')
     $.ajax({
       url: url,
       type: "POST",
@@ -11,5 +30,13 @@ $(function() {
       processData: false,
       contentType: false
     })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.form__message').val('');
+    })
+    .always(() => {
+      $(".form__submit").removeAttr("disabled")
+    });
   })
-})
+});
